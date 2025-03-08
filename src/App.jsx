cf2cea1ee7666530fb08960a7545e1b4b06462f8
KhanhDeddo <khanhdeddo@gmail.com>
2025-03-08 12:41:24 +0700

    remove main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import RouterConfig from './routers/router';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from '~/redux/store'
 import './theme.css'
-
-const App = () => {
-  return (
-    <><RouterConfig/></>
-  );
-}
-export default App;
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterConfig/>
+    </Provider>
+  </StrictMode>
+)
