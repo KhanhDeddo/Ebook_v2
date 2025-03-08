687edd4385cb036bd15115c44703b995e154d1c0
KhanhDeddo <khanhdeddo@gmail.com>
2025-03-08 13:51:10 +0700

    update v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import RouterConfig from './routers/router';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from '~/redux/store'
-import './theme.css'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { Provider } from 'react-redux';
+import store from '~/redux/store';
+import './theme.css';
+
+export default function App() {
+  return (
     <Provider store={store}>
-      <RouterConfig/>
+      <RouterConfig />
     </Provider>
-  </StrictMode>
-)
+  );
+}
